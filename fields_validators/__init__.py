836165e411e1b62f00bfb465088a0cb23d59fd5d
guilherme <guilherme.1995lemes@gmail.com>
2022-03-17 11:07:44 -0300

    fix exceptions __init__

--- fields_validators/__init__.py
+++ fields_validators/__init__.py
@@ -6,7 +6,7 @@ from .regex_validator import RegexValidator
 from .type_validator import TypeValidator
 from .string_validator import StrValidator
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 __all__ = [
     "CPFValidator",
--- fields_validators/exceptions/__init__.py
+++ fields_validators/exceptions/__init__.py
@@ -1,14 +1,5 @@
-from .content_type import ContentTypeSerializerException
-from .enums import ErrorCategory, ErrorCode
-from .general import (
-    BaseError, BaseSerializerException, InvalidFieldError, InvalidTypeError,
-    MissingFieldError, ResourceAlreadyExistsError, ResourceDoesNotExistsError,
-)
-from .terms_contions import NotTermConditionsException
+from .general import InvalidFieldError, InvalidTypeError, MissingFieldError
 
 __all__ = [
-    "ContentTypeSerializerException", "ErrorCategory", "ErrorCode",
-    "BaseSerializerException", "InvalidFieldError", "MissingFieldError",
-    "ResourceAlreadyExistsError", "NotTermConditionsException",
-    "ResourceDoesNotExistsError", "InvalidTypeError", "BaseError"
+    "InvalidFieldError", "InvalidTypeError", "MissingFieldError"
 ]
